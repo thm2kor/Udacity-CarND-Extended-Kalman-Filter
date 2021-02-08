08dc043aee235e4d69ba596d7c5fc6b4ff471df2
thm2kor <thiyagarajan.masilamani@gmail.com>
2021-02-08 17:45:41 +0100

    calculate jacobian as per the class notes

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -39,6 +41,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   Hj << (px/c2), (py/c2), 0, 0,
       -(py/c1), (px/c1), 0, 0,
       py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
-      
+
   return Hj;
 }
